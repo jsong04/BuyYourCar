69a6c50fdd18455c614ad63f10b1751f92b06ad6
JingZheng <song.jingz@husky.neu.edu>
2021-03-29 01:22:36 +0800

    add reusable delete components

--- app/screens/MessageScreen.js
+++ app/screens/MessageScreen.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Platform, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, View, SafeAreaView, StyleSheet, Platform, StatusBar } from 'react-native';
 
 import ListItem from '../components/ListItem';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
 import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
-const messages = [
+const initialMessages = [
     {
         id: 1,
         title: 'T1',
@@ -27,12 +28,22 @@ const messages = [
 ];
 
 function MessageScreen(props) {
+    const [messages, setMessages] = useState(initialMessages);
+
+    const handleDelete = message => {
+        const newMessages = messages.filter(m => m.id !== message.id);
+        setMessages(newMessages);
+    }
+
+
     return (
         <Screen>
             <FlatList data={messages} keyExtractor={(message) => message.id.toString()} renderItem={({item}) => <ListItem 
             title={item.title} 
             subTitle={item.description} 
-            image={item.image} />} 
+            image={item.image}
+            onPress={() => console.log("Message selected")}
+            renderRightActions={() => <ListItemDeleteAction onPress={() => handleDelete(item)} />} />} 
             ItemSeparatorComponent={ListItemSeparator} />
         </Screen>
     );
