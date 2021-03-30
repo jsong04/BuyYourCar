4b0ae3bc4bd3ecdcdefc2bdb6b5a96858bec53f0
JingZheng <song.jingz@husky.neu.edu>
2021-03-30 23:08:38 +0800

    extract errormessage component

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -7,6 +7,7 @@ import Screen from '../components/Screen';
 import AppText from '../components/AppText'
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
+import ErrorMessage from '../components/ErrorMessage';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"),
@@ -20,28 +21,30 @@ function LoginScreen(props) {
             <Image style={styles.logo} source={require('../assets/logo.jpg')} />
 
             <Formik initialValues={{email: '', password: ''}} onSubmit={values => console.log(values)} validationSchema={validationSchema}>
-                {({handleChange, handleSubmit, errors}) => (
+                {({handleChange, handleSubmit, errors, setFieldTouched, touched}) => (
                     <>
                         <AppTextInput 
                             autoCapitalize="none"
                             autoCorrect={false}
                             placeholder="Email"
                             keyboardType="email-address"
+                            onBlur={() => setFieldTouched("email")}
                             textContentType="emailAddress"
                             icon="email"
                             onChangeText={handleChange("email")}
                         />
-                        <AppText style={{color: 'red'}}>{errors.email}</AppText>
+                        <ErrorMessage error={errors.email} visible={touched.email} />
                         <AppTextInput 
                             autoCapitalize="none"
                             autoCorrect={false}
                             placeholder="Password"
                             icon="lock"
+                            onBlur={() => setFieldTouched("password")}
                             textContentType="password"
                             secureTextEntry={true}
                             onChangeText={handleChange("password")}
                         />
-                        <AppText style={{color: 'red'}}>{errors.password}</AppText>
+                        <ErrorMessage error={errors.password} visible={touched.password} />
                         <AppButton title="Login" onPress={handleSubmit} />
                     </>
                 )}
