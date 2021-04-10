e1e88b10b30bfffeb9cbb33ae0ed130208785273
JingZheng <song.jingz@husky.neu.edu>
2021-04-10 18:06:45 +0800

    connect to backend fake data

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ import AppNavigator from './app/navigation/AppNavigator';
 export default function App() {
   return (
     <NavigationContainer theme={NavigationTheme}>
-      <AuthNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
 }
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,33 +1,65 @@
-import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, StyleSheet } from 'react-native';
 import Screen from '../components/Screen';
 import Card from '../components/Card';
 
 import colors from '../config/colors';
+import listingsApi from '../api/listings';
+import axios from 'axios';
+import Constants from 'expo-constants';
+import AppText from '../components/AppText';
+import AppButton from '../components/AppButton';
+const { manifest } = Constants;
 
-const listings = [
-    {
-        id: 1,
-        title: "Audi",
-        price: 39999,
-        image: require('../assets/Audi_R8.jpg')
-    },
-    {
-        id: 2,
-        title: "Civic",
-        price: 12999,
-        image: require('../assets/civic.jpg')
-    }
-];
+// const listings = [
+//     {
+//         id: 1,
+//         title: "Audi",
+//         price: 39999,
+//         image: require('../assets/Audi_R8.jpg')
+//     },
+//     {
+//         id: 2,
+//         title: "Civic",
+//         price: 12999,
+//         image: require('../assets/civic.jpg')
+//     }
+// ];
 
 function ListingsScreen({ navigation }) {
+    const [listings, setListings] = useState([]);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        load();
+    }, []);
+
+    const load = async () => {
+        setLoading(true);
+        const res = await listingsApi.getListings();
+        if (!res.ok) return setError(true);
+        setError(false);
+        // const res = await axios.get("http://192.168.1.4:7000/api/listings");
+        setLoading(false);
+        if (res) {
+            console.log(res);
+            setListings(res.data);
+        }
+    }
+
     return (
         <Screen style={styles.screen}>
+            {error && <>
+                <AppText>No Internet Connection</AppText>
+                <AppButton title="Reload" onPress={load} />
+            </>}
+            <ActivityIndicator animating={loading} size="large"/>
             <FlatList showsVerticalScrollIndicator={false} data={listings} keyExtractor={listing => listing.id.toString()} renderItem={({ item }) => {
                 return <Card 
                         title={item.title} 
                         subTitle={"$" + item.price} 
-                        image={item.image}
+                        imageUrl={item.images[0].url}
                         onPress={() => navigation.navigate("ListingDetails", item)}
                           />
             }} />
